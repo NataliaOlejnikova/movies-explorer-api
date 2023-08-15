22c2d3ec55d0acecf3f49e59dc82518526beb6c5
NataliaOlejnikova <olejnikovanatalia89@yandex.ru>
2023-08-15 12:33:33 +0700

    1versia

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const cors = require('cors');
+const { errors } = require('celebrate');
+const { routes } = require('./routes');
+const { limiter } = require('./middlewares/limiter');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+require('dotenv').config();
+const { PORT = 3000, NODE_ENV, DB_PROD } = process.env;
+
+const app = express();
+
+async function main() {
+   await mongoose.connect(NODE_ENV === 'production' ? DB_PROD : 'mongodb://localhost:27017/bitfilmsdb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  app.use(cors());
+
+   app.use(requestLogger);
+
+    app.use(limiter);
+
+    app.use(routes);
+
+    app.use(errorLogger);
+
+    app.use(errors());
+
+    app.use((err, req, res, next) => {
+        const { statusCode = 500, message } = err;
+    res
+      .status(statusCode)
+      .send({
+               message: statusCode === 500
+          ? 'На сервере произошла ошибка'
+          : message,
+      });
+    next();
+  });
+
+  await app.listen(PORT);
+}
+
+main();
---
+++ middlewares/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const TokenError = require('../errors/wrong-token-err');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+module.exports = (req, res, next) => {
+    const { authorization } = req.headers;
+    if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new TokenError('Ошибка авторизации: неправильная почта или логин');
+  }
+    const token = authorization.replace('Bearer ', '');
+    let payload;
+  try {
+          payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+  } catch (err) {
+        throw new TokenError('Ошибка авторизации: не получилось верифицировать токен');
+  }
+
+  req.user = payload;
+
+  next();
+};
