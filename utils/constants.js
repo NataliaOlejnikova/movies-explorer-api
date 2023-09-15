adb4ff64e84ccd2afbba41f461147c54d78310fd
NataliaOlejnikova <olejnikovanatalia89@yandex.ru>
2023-09-15 16:07:27 +0700

    new

--- utils/constants.js
+++ utils/constants.js
@@ -8,21 +8,4 @@ const allowedCors = [
   /(https|http)?:\/\/(?:www\.|(?!www))react-mesto-backend.nomoreparties.co\/[a-z]+\/|[a-z]+\/|[a-z]+(\/|)/,
 ];
 
-const errorMessages = {
-  image: 'неправильный формат ссылки на картинку',
-  trailerLink: 'неправильный формат ссылки на трейлер',
-  thumbnail: 'неправильный формат ссылки на постер',
-  createMovie: 'неправильные данные при создании фильма',
-  movieNotFound: 'Фильм не найден',
-  removeMovie: 'фильм другого пользователя удалить нельзя',
-  userNotFound: 'пользователь не существует',
-  createUser: 'пользователь уже существует',
-  updateProfile: 'email с таким именем уже зарегистрирован',
-  incorrectData: 'неправильный логин или пароль',
-  incorrectEmail: 'некорректный формат почты',
-  incorrectPath: 'неправильный путь',
-  auth: 'необходима авторизация',
-  crash: 'сервер сейчас перегружен',
-};
-
-module.exports = { allowedCors, errorMessages };
+module.exports = { allowedCors};
