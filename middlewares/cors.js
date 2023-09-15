35750157f930f1d752b169093421ffffa767db74
NataliaOlejnikova <olejnikovanatalia89@yandex.ru>
2023-09-15 15:10:43 +0700

    new

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -4,7 +4,7 @@ const allowedCors = [
   'localhost:3000',
   'http://localhost:3000',
   'http://127.0.0.1:3000',
-  /(https|http)?:\/\/(?:www\.|(?!www))movies-api.nomoredomains.xyz\/[a-z]+\/|[a-z]+\/|[a-z]+(\/|)/,
+  /(https|http)?:\/\/(?:www\.|(?!www))react-mesto-backend.nomoreparties.co\/[a-z]+\/|[a-z]+\/|[a-z]+(\/|)/,
 ];
 
 module.exports = (req, res, next) => {
